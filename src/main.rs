b0fc4a996bf840b495d000c50ab10379cd72182c
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2023-05-30 04:51:31 -0700

    `fm (v0.1.0@beta)` structure init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::env::args;
+use std::process::exit;
+
+
+struct Fm {
+    command: String,
+    path:    String,
+    data:    String,
+}
+
+
+fn main()  {
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 4 {
+        param();
+    }
+
+
+    let mut fm = Fm {
+        command: args[1].clone(),
+        path: args[2].clone(),
+        data: args[3].clone(),
+    };
+
+    match fm.command.as_str() {
+        "fread" => {
+            println!("Ok: fread");
+        }
+        "fwrite" => {
+            println!("Ok: fwrite");
+        }
+        "ffind" => {
+            println!("Ok: ffind");
+        }
+        "fcreate" => {
+            println!("Ok: fcreate");
+        }
+        "fremove" => {
+            println!("Ok: fremove");
+        }
+        _ => {
+            eprintln!("Unknown command.");
+            param();
+        }
+    }
+
+}
+
+fn param() {
+    eprintln!("$ Usage:  <command> <path> <data>");
+    eprintln!("Commands:\n\tfwrite\n\tfread\n\tffind\n\tfcreate\n\tfremove");
+    exit(1);
 }
