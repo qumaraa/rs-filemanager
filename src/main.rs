dfca229735aea683e8c167d58557f75af6289f06
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2023-05-30 07:13:26 -0700

    `@alpha0.2` - `fread` and `fwrite` def-ed

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,66 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
 use std::env::args;
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead, BufReader, BufWriter, Read, Write};
 use std::process::exit;
-
 
 struct Fm {
     command: String,
-    path:    String,
-    data:    String,
+    path: String,
+    data: Option<String>,
 }
 
-
-fn main()  {
+fn main() {
     let args: Vec<String> = args().collect();
 
-    if args.len() < 4 {
+    if args.len() < 3 || args.len() > 4 {
         param();
     }
-
-
     let mut fm = Fm {
         command: args[1].clone(),
         path: args[2].clone(),
-        data: args[3].clone(),
+        data: if args.len() > 3 {
+            Some(args[3].clone())
+        } else {
+            None
+        },
     };
-
     match fm.command.as_str() {
         "fread" => {
-            println!("Ok: fread");
+            let mut fs: File = match File::open(&fm.path) {
+                Ok(f) => f,
+                Err(why) => {
+                    println!("[rustfm] Error: {why}");
+
+                    return ();
+                }
+            };
+            let mut buf = String::new();
+            match fs.read_to_string(&mut buf) {
+                Ok(i) => i,
+                Err(why) => {
+                    println!("[rustfm] Error: {why}");
+                    return ();
+                }
+            };
+            println!("{buf}");
         }
         "fwrite" => {
-            println!("Ok: fwrite");
+            // using `unwrap()` while testing.
+            // in future, of course, we'll handle the Errors
+
+            let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(&fm.path)
+                .unwrap();
+
+            if let Some(data) = fm.data {
+                file.write_all(data.as_bytes()).unwrap();
+            }
+            println!(
+                "[rustfm] Data has been successfully written in {}",
+                &fm.path
+            );
         }
         "ffind" => {
             println!("Ok: ffind");
@@ -42,11 +72,9 @@ fn main()  {
             println!("Ok: fremove");
         }
         _ => {
-            eprintln!("Unknown command.");
             param();
         }
     }
-
 }
 
 fn param() {
